e78241504dc82be5e4c3f6bf18c54765ad8c4651
Einar Avdem <einaravdem@Einars-MBP.lan>
2020-11-01 20:34:47 +0100

    More modification of MATLAB script

--- AIM-Spice/figures/plots.m
+++ AIM-Spice/figures/plots.m
@@ -1,6 +1,9 @@
+%Plotting script
 close all;
 clc;
 
+%%
+%Loading datafile and allocating variables
 doc = load('test750p2m.txt');
 
 t = doc(:,1);
@@ -10,6 +13,8 @@ v_nre1r1 = doc(:,5);
 vout_sampled = doc(:,6);
 vout = doc(:,7);
 
+%%
+%First plot pane
 subplot(5, 1, 1);
 plot(t*1e3, v_erease, 'r', 'linewidth', 1.5);
 axis([0 12 -0.5 2]);
@@ -17,6 +22,8 @@ grid;
 ylabel('V_{erease} [V]', 'fontsize', 14);
 xlabel('Time [ms]', 'fontsize', 14);
 
+%%
+%First plot pane
 subplot(5, 1, 2);
 plot(t*1e3, v_expose, 'r', 'linewidth', 1.5);
 axis([0 12 -0.5 2]);
@@ -24,6 +31,8 @@ grid;
 ylabel('V_{expose} [V]', 'fontsize', 14);
 xlabel('Time [ms]', 'fontsize', 14);
 
+%%
+%First plot pane
 subplot(5, 1, 4);
 plot(t*1e3, v_nre1r1, 'r', 'linewidth', 1.5);
 axis([0 12 -0.5 2]);
@@ -31,6 +40,8 @@ grid;
 ylabel('V_{nre1r1} [V]', 'fontsize', 14);
 xlabel('Time [ms]', 'fontsize', 14);
 
+%%
+%First plot pane
 subplot(5, 1, 5);
 plot(t*1e3, vout, 'r', 'linewidth', 1.5);
 axis([0 12 0 2]);
@@ -38,9 +49,15 @@ grid;
 ylabel('V_{out} [V]', 'fontsize', 14);
 xlabel('Time [ms]', 'fontsize', 14);
 
+%%
+%First plot pane
 subplot(5, 1, 3);
 plot(t*1e3, vout_sampled, 'r', 'linewidth', 1.5);
 axis([0 12 -0.5 1.5]);
 grid;
 ylabel('V_{out sampled} [V]', 'fontsize', 14);
 xlabel('Time [ms]', 'fontsize', 14);
+
+%%
+%Command for exporting vector graphics:
+%exportgraphics(gcf,'node_voltages_@_Id750_and_texp2ms.pdf','ContentType','vector'
