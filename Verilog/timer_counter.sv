3c8678909c5a3c6402303c542be22b9dcddc3e60
Øyvind Skaaden <oyvindskaaden@gmail.com>
2020-11-15 15:49:09 +0100

    Rename set_ex to ex_set

--- Verilog/timer_counter.sv
+++ Verilog/timer_counter.sv
@@ -1,6 +1,6 @@
 module timer_counter (
 	input 	logic					clk,
-	input 	logic 				setTime,
+	input 	logic 				ex_set,
 	input 	logic 				enable,
 	input 	logic [4:0] 	init,
 	output	wire 					ex_done 
@@ -9,7 +9,7 @@ module timer_counter (
 	logic [4:0] internal_count;
 
 	always @(posedge clk)
-		if (setTime)
+		if (ex_set)
 			internal_count = init + 1; //load internal_count with new values, add one because the way it counts
 		else if (enable && (internal_count > 0))
 			internal_count = internal_count-1;
@@ -22,11 +22,11 @@ endmodule
 
 module timer_counter_tb();
 
-	logic clk, setTime, enable;
+	logic clk, ex_set, enable;
 	wire ex_done;
 	logic [4:0] init;
 
-	timer_counter testbench(clk, setTime, enable, init, ex_done);
+	timer_counter testbench(clk, ex_set, enable, init, ex_done);
 
 
 	always begin
@@ -37,15 +37,15 @@ module timer_counter_tb();
 		$dumpfile("outfiles/timer_counter/timer_counter_tb.vcd");
 		$dumpvars();
 
-		{clk, enable, setTime} = 3'b100;
+		{clk, enable, ex_set} = 3'b100;
 		init = 5'd10;
-		#2 {enable, setTime} = 2'b01;
-		#2 {enable, setTime} = 2'b10;
+		#2 {enable, ex_set} = 2'b01;
+		#2 {enable, ex_set} = 2'b10;
 	end
 
 	always @(posedge ex_done) begin
-		#2 {enable, setTime} = 2'b00;
-		#4 {enable, setTime} = 2'b01;
+		#2 {enable, ex_set} = 2'b00;
+		#4 {enable, ex_set} = 2'b01;
 		#4 $finish;
 	end
 endmodule
